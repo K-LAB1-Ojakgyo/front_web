9b937d1fa1a6ab2943aeeff2692287fc5622b32f
kungyadami <yedam7989@naver.com>
2023-05-28 02:06:56 +0900

    modification book_choice.js

--- js/book_choice.js
+++ js/book_choice.js
@@ -5,6 +5,15 @@ function showRandomBooks() {
      * 각 보여지는 책들은 radio 버튼처럼 하나를 픽하게끔 되어야 하고
      * 하나가 골라지는 순간 select 버튼이 활성화가 되어야 함
      */
+    var image_array=new Array(4);
+    image_array=getRandomBooks(); //random하게 가져오기
+    for(var i=0;i<4;i++){
+        var temp_string = "#book"+(i+1); //string을 통해 id가져오기
+        var img_id=$(temp_string); //image에 이 id가 들어있음
+        $(img_id).attr("src",image_array[i]);
+    }
+
+    
 }
 
 function refreshBtnClicked() {
@@ -17,4 +26,12 @@ function logoutBtnClicked() {
 
 function selectBtnClicked() {
     // 완료 다이얼로그가 뜨면서 5초 후 login 화면으로 이동
+}
+
+function addClickEffect(element){
+    var images = document.getElementsByClassName('book');
+    for(var i=0; i<images.length ;i++){
+        images[i].classList.remove("clicked");
+    }
+    element.classList.add("clicked");
 }
