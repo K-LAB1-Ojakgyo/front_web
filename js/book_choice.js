e8a10acb9ad723ce5915606d869718852303bb59
kungyadami <yedam7989@naver.com>
2023-05-28 15:04:48 +0900

    add dialog

--- js/book_choice.js
+++ js/book_choice.js
@@ -1,21 +1,14 @@
 var isClicked=0;
 
 function showRandomBooks() {
-    /**
-     * util의 getRandomBooks를 활용하여 정보를 얻어온후
-     * 보여주기
-     * 각 보여지는 책들은 radio 버튼처럼 하나를 픽하게끔 되어야 하고
-     * 하나가 골라지는 순간 select 버튼이 활성화가 되어야 함
-     */
     var image_array=new Array(4);
     image_array=getRandomBooks(); //random하게 가져오기
     for(var i=0;i<4;i++){
         var temp_string = "#book"+(i+1); //string을 통해 id가져오기
         var img_id=$(temp_string); //image에 이 id가 들어있음
         $(img_id).attr("src",image_array[i]);
+        $(img_id).removeClass('clicked');
     }
-
-    
 }
 
 function refreshBtnClicked() {
@@ -28,12 +21,29 @@ function logoutBtnClicked() {
 
 function selectBtnClicked() {
     // 완료 다이얼로그가 뜨면서 5초 후 login 화면으로 이동
+
     if(isClicked==1){
-        /*완료 다이얼로그*/
-        $('#book_choice_select').click(function(){
-            $('#book_choice_dialog').dialog();
+        var idCheckDialog = $("#book_choice_dialog")[0];
+        var infoNode = "choicing the book is completed!";
+        $("#dialog_title").empty();
+        $("#dialog_title").append(infoNode);
+        idCheckDialog.showModal();
+        $('#book_choice_login_or_back').attr("src", "./book_img/login_button.png");
+        $('#book_choice_login_or_back').click(function(){
+           window.location.href="login.html";
+        });
+    }else{
+        var idCheckDialog = $("#book_choice_dialog")[0];
+        var infoNode = "Please choose the book you want to read.";
+        $("#dialog_title").empty();
+        $("#dialog_title").append(infoNode);
+        idCheckDialog.showModal();
+        $('#book_choice_login_or_back').attr("src", "./book_img/back_button.png");
+        $('#book_choice_login_or_back').click(function(){
+            idCheckDialog.close();
         });
     }
+
 }
 
 function addClickEffect(element){
