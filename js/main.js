575a6375ece0129d1ce4086310ea4bab89c49c00
이찬양 <cylee@ichan-yang-ui-MacBookAir.local>
2023-06-01 11:19:02 +0900

    style.css logout

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function showBadges() {
      * util의 getRecentBadges를 활용하여 정보를 받아와서
      * 화면에 띄워줌
      */
-    var badges = ["res/b0001.png","res/b0002.png", "res/b0003.png"]; // 더미데이터
+    var badges = ["res/dummy/badge1.png","res/dummy/badge2.png", "res/dummy/badge3.png"]; // 더미데이터
     $("#badgeBack1").attr("src", badges[0]);
     $("#badgeBack2").attr("src", badges[1]);
     $("#badgeBack3").attr("src", badges[2]);
@@ -26,7 +26,7 @@ function showQuests() {
      * util의 getRecentQuests를 활용하여 정보를 받아와서
      * 화면에 띄워줌
      */
-    var quests = ["res/000001.png","res/000002.png", "res/000003.png"]; // 더미데이터
+    var quests = ["res/dummy/book1.png","res/dummy/book2.png", "res/dummy/book3.png"]; // 더미데이터
     $("#bookBack1").attr("src", quests[0]);
     $("#bookBack2").attr("src", quests[1]);
     $("#bookBack3").attr("src", quests[2]);
@@ -37,7 +37,7 @@ function showChallenge() {
      * util의 getNowchallenge를 활용하여 정보를 받아와서
      * 화면에 띄워줌
      */
-    var nowChallenge = "res/000001.png";
+    var nowChallenge = "res/dummy/book1.png";
     $("#challengeBack").attr("src", nowChallenge);
 }
 
@@ -52,24 +52,21 @@ function showBookTitle() {
 function quizBtnClicked() {
     // 퀴즈 화면으로 이동
     $('#quizBtn').click(function(){
-        alert('퀴즈이동');
-				window.location.href = "quiz.html";
+		window.location.href = "quiz.html";
     });
 }
 
 function moreBtnClicked() {
     // 배지 화면으로 이동
     $('#moreBtn').click(function(){
-        alert('배지이동');
-				window.location.href = "badge.html";
+		window.location.href = "badge.html";
     });
 }
 
 function chooseBookBtnClicked() {
     // 책 선택 화면으로 이동
     $('#refreshBtn').click(function(){
-        alert('책선택이동');
-				window.location.href = "book_choice.html";
+		window.location.href = "book_choice.html";
     });
 }
 
